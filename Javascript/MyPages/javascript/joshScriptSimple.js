a6a4e85ecc17ec937ec7eaafb41e54593015fd16
Gazpacho <Gazpachoblaze@gmail.com>
2022-03-15 21:57:03 -0600

    added web layout design with two rows of columns

---
+++ Javascript/MyPages/javascript/joshScriptSimple.js
@@ -0,0 +1,12 @@
+/*Developer; Josh
+Date 2/4/22
+
+
+*/
+
+let ownerName = "Gazpacho Blaze";
+if (ownerName === "Gazpacho Blaze") alert("GAZPACHO IS THE KING");
+let oneName = "Super Zebra";
+
+const personName = prompt("What's your name?");
+document.write('<h2>This is from Javascript</h2>');
---
+++ archives/joshscript_old.js
@@ -0,0 +1,200 @@
+/*Developer; Josh
+Date 2/4/22
+
+
+*/
+
+let ownerName = "Gazpacho Blaze";
+if (ownerName === "Gazpacho Blaze") alert("GAZPACHO IS THE KING");
+let oneName = "Super Zebra";
+
+
+
+console.log(oneName);
+console.log(oneName);
+console.log(oneName);
+
+
+let javascriptsss = true;
+console.log(javascriptsss);
+
+
+console.log(typeof true);
+console.log(typeof oneName);
+
+
+const today = new Date();
+let currentYear = today.getFullYear();
+
+
+alert(today);
+alert(currentYear);
+
+
+const personName = prompt("What's your name?");
+alert(`Hello ${personName} and welcome!!!`);
+
+const personBirthYear = prompt("What year were you born?");
+alert(` ${personBirthYear} ?? cool! `);
+
+
+const personCurrentAge = currentYear - personBirthYear;
+alert(`You are ${personCurrentAge}`);
+
+const firstName = "Gazpacho";
+const lastName = "Blaze";
+console.log(firstName + " " + lastName);
+
+let Jh = 10 + 7;
+Jh += 16;
+Jh *= 4;
+console.log(Jh);
+
+
+
+
+
+const massJosh = 70;
+const heightJosh = 1.69;
+const massCube = 92;
+const heightCube = 1.95;
+
+const BMIJosh = massJosh / heightJosh ** 2;
+const BMICube = massCube / (heightCube * heightCube);
+
+console.log(BMIJosh, BMICube);
+
+const CubeBMIhigher = BMICube > BMIJosh;
+console.log(BMIJosh, BMICube, CubeBMIhigher);
+
+const name1 = "Gazpacho";
+const name2 = "Inventor";
+const Birthyear = 2000;
+const YearNow = 2022;
+
+const Gaz = "I'm" + " " + name1 + " " + "and i work as an inventor who was born in" + " " + Birthyear + " " + "which means that I am" + " " + (YearNow - Birthyear);
+console.log(Gaz);
+
+const GazNew = `I'm ${name1}, a ${YearNow - Birthyear} year old ${name2}`;
+
+console.log(GazNew);
+
+
+let GameCube = "Nintendo GameCube";
+console.log(GameCube);
+
+let CubeOld = (currentYear - 1994);
+console.log(CubeOld);
+
+
+
+let save = 10 + 10;
+save += 80;
+save *= 10;
+console.log(save);
+
+let wow = "text that I finished";
+let oooo = "do it now";
+let textfile = `This is my 1st ${wow} and I know how ${oooo}`;
+
+console.log(textfile);
+
+
+const tree = "free to do whatever";
+const beginnerz = `I am ${tree}`;
+console.log(beginnerz);
+
+
+
+const fire = "The fire is hot";
+const water = "The water is cold";
+const MixElement = `${fire} but ${water}`;
+
+console.log(MixElement);
+
+let January = "This" + " " + "month" + " " + "was" + " " + "rough";
+console.log(January);
+
+let xx = 10;
+xx += 40;
+xx *= 4;
+xx /= 200;
+console.log(xx);
+
+
+console.log(`regular string`);
+console.log(`this has two lines \n\
+so its \n\
+different`);
+
+const legalAge = 16;
+const isOldEnough = personCurrentAge >= legalAge;
+
+if (isOldEnough) {
+    console.log(`You can start driving`);
+    alert(`You can start driving`);
+
+
+} else {
+    const yearsLeft = legalAge - personCurrentAge;
+    console.log(yearsLeft);
+    console.log(`You are too young to drive so wait ${yearsLeft} more years`);
+    alert(`You are too young to drive so wait ${yearsLeft} more years`);
+}
+
+const zebraYear = 1994;
+const blazeYear = 2000;
+let century;
+if (zebraYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+
+}
+
+console.log(zebraYear);
+console.log(blazeYear);
+
+let globe = "Look at Earth";
+console.log(`You Need To ${globe}`);
+
+
+const Turtle27 = 27;
+let meh;
+if (Turtle27 >= 27) {
+    meh = "This turtle is old";
+} else {
+    meh = "This turtle is young";
+}
+
+console.log(meh);
+
+const IWR = 17;
+let duhh;
+if (IWR === 17) {
+    duhh = "I Was Right";
+} else {
+    duhh = "I Was Wrong";
+}
+
+console.log(duhh);
+
+const welp = "100k" + " " + "is yours" + " " + "if you focus";
+
+const WakeUp = `${welp}`;
+console.log(WakeUp);
+
+
+
+let huge;
+if (BMIJosh <= BMICube) {
+    huge = `Gazpacho Has More BMI`;
+} else {
+    huge = `Cube Has More BMI`;
+}
+
+console.log(huge);
+
+
+
+
