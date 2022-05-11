31fe81addfc5535e9fd6b7ae5d956a06ffa5c2d1
Gazpacho <Gazpachoblaze@gmail.com>
2022-05-11 17:06:53 -0500

    adding new practice page and scripts js code

---
+++ Javascript/Lesson1/js/scripts.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* 
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive");
+
+const interface = "audio";
+
+*/
+
+function logger() {
+    console.log("My name is Gazpacho");
+}
+
+
+/* Calling / Running / Invoking / the function */
+logger();
--- Javascript/MyPages/javascript/joshScriptSimple.js
+++ Javascript/MyPages/javascript/joshScriptSimple.js
@@ -41,3 +41,13 @@ switch (day) {
     default:
         console.log("Not a valid day");
 }
+
+/* Challenge 4 */
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 :
+bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
+
+/* Activating strict mode*/
+
+
