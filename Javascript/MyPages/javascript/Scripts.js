b5b9b87ba55a684d3d97e5137049f29f079cf7d7
Gazpacho <Gazpachoblaze@gmail.com>
2022-05-16 18:29:56 -0500

    functions

--- Javascript/MyPages/javascript/Scripts.js
+++ Javascript/MyPages/javascript/Scripts.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 let ownerName = "Gazpacho Blaze";
 if (ownerName === "Gazpacho Blaze") alert("GAZPACHO IS THE KING");
@@ -61,8 +61,7 @@ console.log(String(17), 17);
 document.write(` "I am" + 24 + "years old"`);
 console.log("23" * "4");
 
-//boolean//
-document.write(boolean);
+
 
 
 
@@ -86,6 +85,50 @@ function logger() {
 
 /* Calling / Running / Invoking / the function */
 logger();
+logger();
+logger();
+logger();
+
+function plants(mangoes, peaches) {
+    const juice = (`juice with ${mangoes} mangoes and ${peaches} peaches.`);
+    return juice;
+}
+
+const mangojuice = plants(5,0);
+console.log(mangojuice);
+console.log(plants(5,0));
+
+MangoPeachJuice = plants(7,8);
+console.log(MangoPeachJuice);
+
+/* Function declarations */
+
+function calcAge1(birthdayYear) {
+    return 2021 - birthdayYear;
+}
+
+const age1 = calcAge1(1994);
+console.log(age1);
 
 
+const calcAge2 = function (birthdayYear) {
+    return 2021 - birthdayYear;
+}
+
+const age2 = calcAge2(1994);
+console.log(age1,age2);
+
+/* Arrow Functions */
+
+const calcAge3 = birthdayYear => 2021 - birthdayYear;
+const age3 = calcAge3(1994);
+console.log(age3);
+
+const retirementYears = birthdayYear => {
+    const age = 2021 - birthdayYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(retirementYears(1994, "Gazpacho"));
 
